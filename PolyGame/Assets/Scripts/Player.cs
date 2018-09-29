67277c3db3918c0fd71e3706b3778fdd5e796bf7
Psheldon <psheldon@mail.pima.edu>
2018-09-28 23:12:34 -0700

    Made first scripts, and prefabs to hold them.

---
+++ PolyGame/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Unit {
+
+    public int level;
+    public int exp;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
---
+++ PolyGame/Assets/Scripts/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+
+    public int health;
+    public int speed;
+    public int attack;
+    public int defense;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
