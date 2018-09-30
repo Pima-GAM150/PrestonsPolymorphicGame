9742e738e8354d08ccecd804f49a2b7b791e36ed
Psheldon <psheldon@mail.pima.edu>
2018-09-29 17:40:12 -0700

    Added player movement, started singleton script.

--- PolyGame/Assets/Scripts/Player.cs
+++ PolyGame/Assets/Scripts/Player.cs
@@ -6,15 +6,32 @@ public class Player : Unit {
 
     public int level;
     public int exp;
+    public new Transform transform;
+    private float xInput;
+    private float yInput;
 
     void Start()
     {
-
+        
     }
 
     void Update()
     {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+        PlayerMove();
+    }
 
+    public void PlayerMove()
+    {
+        if(xInput != 0)
+        {
+            transform.position = new Vector3(transform.position.x + (speed * xInput * Time.deltaTime), transform.position.y);
+        }
+        if(yInput != 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + (speed * yInput* Time.deltaTime));
+        }
     }
 
 }
