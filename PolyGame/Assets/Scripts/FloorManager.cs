42bfa078a314f19422c4920b60fb1da2438beb24
Psheldon <psheldon@mail.pima.edu>
2018-09-29 17:58:38 -0700

    Added manager prefab, additional scene, and an intrange class for random

--- PolyGame/Assets/Scripts/FloorManager.cs
+++ PolyGame/Assets/Scripts/FloorManager.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class FloorManager : MonoBehaviour {
+
+    private FloorManager manager;
+    
+    //The type of tile that will be held in a position.
+    public enum TileType
+    {
+        Wall, Floor,
+    }
+
+    public int columns = 50;
+    public int rows = 50;
+    public IntRange numRooms = new IntRange(0, 0);
+    public IntRange roomWidth = new IntRange(0, 0);
+
+     
 
 	// Use this for initialization
 	void Start () {
-		
+        //Ensures that only one manager can be loaded.
+		if(manager == null)
+        {
+            manager = this;
+            DontDestroyOnLoad(manager);
+        }
+        else
+        {
+            Destroy(this);
+        }
 	}
 	
 	// Update is called once per frame
---
+++ PolyGame/Assets/Scripts/IntRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class IntRange {
+    //The minimum and maximum values in this range.
+    public int m_Min;
+    public int m_Max;
+
+    //Constructer to set values
+    public IntRange(int min, int max)
+    {
+        m_Min = min;
+        m_Max = max;
+    }
+
+    //Get a random value from this range.
+    public int Random
+    {
+        get { return UnityEngine.Random.Range(m_Min, m_Max); }
+    }
+}
